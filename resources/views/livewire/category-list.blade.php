06e9a7ef99eb20311df9387a5da732af0bc267f3
Reza Lael <rezalaal@gmail.com>
2024-04-25 09:56:15 +0330

    changed menu icon size

--- resources/views/livewire/category-list.blade.php
+++ resources/views/livewire/category-list.blade.php
@@ -3,11 +3,11 @@
         <div class="mt-4 cursor-pointer ">
             @if ($category->getFirstMediaUrl() == null)
                 <a href="/products/{{ $category->id }}" wire:navigate>
-                    <img class="rounded-full w-32 h-32 md:w-60 md:h-60" src="{{ config('app.url').'/images/category.jpg' }}" alt="Menu Picture">    
+                    <img class="rounded-full w-28 h-28 md:w-60 md:h-60" src="{{ config('app.url').'/images/category.jpg' }}" alt="Menu Picture">    
                 </a>
             @else
                 <a href="/products/{{ $category->id }}" wire:navigate>
-                    <img class="rounded-full w-32 h-32 md:w-60 md:h-60" src="{{ $category->getFirstMediaUrl() }}" alt="Menu Picture">    
+                    <img class="rounded-full w-28 h-28 md:w-60 md:h-60" src="{{ $category->getFirstMediaUrl() }}" alt="Menu Picture">    
                 </a>
             @endif
             
