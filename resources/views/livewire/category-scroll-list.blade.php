633d55441dc6a28250cc873cb4468158750efff4
Reza Lael <rezalaal@gmail.com>
2025-05-31 17:43:44 +0330

    fixed ui

--- resources/views/livewire/category-scroll-list.blade.php
+++ resources/views/livewire/category-scroll-list.blade.php
@@ -1,6 +1,6 @@
 <div class="flex flex-col">
-    <ul class="text-3xl flex scroll-smoth snap-start overflow-x-scroll min-h-0 m-4 ">
-        @foreach ($categories as $category)
+    <ul class="flex text-3xl scroll-smooth snap-start overflow-x-auto min-h-0 m-4 no-scrollbar">
+    @foreach ($categories as $category)
             @if($category->id == $categoryId)
             <li wire:click="category({{ $category->id }})" class="bg-lime-100 font-iransans-ultralight text-black text-sm cursor-pointer px-4 py-2 rounded-xl shadow-md flex flex-row mx-2 whitespace-nowrap">{{ $category->name }}</li>
             @else
--- storage/framework/views/d3c52eb60d02f9d9de3e8659db25af86.php
+++ storage/framework/views/d3c52eb60d02f9d9de3e8659db25af86.php
@@ -1,6 +1,6 @@
 <div class="flex flex-col">
-    <ul class="text-3xl flex scroll-smoth snap-start overflow-x-scroll min-h-0 m-4 ">
-        <!--[if BLOCK]><![endif]--><?php $__currentLoopData = $categories; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $category): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+    <ul class="flex text-3xl scroll-smooth snap-start overflow-x-auto min-h-0 m-4 no-scrollbar">
+    <!--[if BLOCK]><![endif]--><?php $__currentLoopData = $categories; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $category): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
             <!--[if BLOCK]><![endif]--><?php if($category->id == $categoryId): ?>
             <li wire:click="category(<?php echo e($category->id); ?>)" class="bg-lime-100 font-iransans-ultralight text-black text-sm cursor-pointer px-4 py-2 rounded-xl shadow-md flex flex-row mx-2 whitespace-nowrap"><?php echo e($category->name); ?></li>
             <?php else: ?>
