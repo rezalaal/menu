dadd5d330ee786387546db2b7a78f348f33f3732
Reza Lael <rezalaal@gmail.com>
2025-05-31 12:09:10 +0330

    testItem

--- app/Filament/Resources/TestItemResource.php
+++ app/Filament/Resources/TestItemResource.php
@@ -4,6 +4,8 @@ namespace App\Filament\Resources;
 
 use App\Filament\Resources\TestItemResource\Pages;
 use App\Filament\Resources\TestItemResource\RelationManagers;
+use Filament\Tables\Columns\SpatieMediaLibraryImageColumn;
+use Filament\Forms\Components\SpatieMediaLibraryFileUpload;
 use App\Models\Category;
 use Filament\Forms;
 use Filament\Forms\Form;
@@ -17,13 +19,30 @@ class TestItemResource extends Resource
 {
     protected static ?string $model = Category::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-list-bullet';
+    protected static ?string $label = "category";
+    protected static ?string $pluralLabel = "categories";
 
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
-                //
+                Forms\Components\TextInput::make('name')
+                    ->label('عنوان دسته بندی')
+                    ->required(),
+                SpatieMediaLibraryFileUpload::make('image')  
+                    ->beforeStateDehydrated(function() {
+                        info("image upload dehydrated");
+                    })
+                    ->label('تصویر')                  
+                    // ->conversion('thumb')
+                    ->afterStateUpdated(function ($state) {
+                        info('Uploaded file info:', ['state' => $state]);
+                    }),
+                Forms\Components\TextInput::make('sort_order')
+                    ->label('ترتیب')
+                    ->integer()
+                    ->rules(['nullable', 'numeric','min:0'])
             ]);
     }
 
@@ -31,7 +50,23 @@ class TestItemResource extends Resource
     {
         return $table
             ->columns([
-                //
+                SpatieMediaLibraryImageColumn::make('image')
+                    ->circular()
+                    ->label('تصویر'),
+                    // ->conversion('thumb'),
+                Tables\Columns\TextColumn::make('name')
+                    ->label('عنوان دسته بندی')
+                    ->searchable(),                
+                Tables\Columns\TextColumn::make('created_at')
+                    ->label('تاریخ ایجاد')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->label('تاریخ ویرایش')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
             ])
             ->filters([
                 //
