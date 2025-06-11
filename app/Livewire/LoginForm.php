85a5e1ebadd9e12d0cd17b9df0cb9edc14e56620
Reza Lael <rezalaal@gmail.com>
2025-06-11 22:38:55 +0330

    verify code

--- app/Livewire/LoginForm.php
+++ app/Livewire/LoginForm.php
@@ -10,6 +10,12 @@ use Livewire\Attributes\Validate;
 class LoginForm extends Component
 {
 
+    #[Validate(['otp' => 'required|digits:5'],message:[
+        'required' => 'کد تایید الزامی است',
+        'digits' => 'کد تایید ۵ رقمی است'
+    ])]
+    public $otp;
+
     #[Validate(['mobile' => 'required|regex:/^09[0-9]{9}$/'],message:[
         'required' => 'شماره تلفن همراه خود را وارد کنید',
         'regex' => 'شماره تلفن وارد شده نامعتبر است'
@@ -27,18 +33,50 @@ class LoginForm extends Component
 
     public function SendOtp()
     {
-        $this->validate();
 
+        $this->validateOnly('mobile');
+        $otpCode = random_int(10000, 99999);
+        session([
+            'otp' => [
+                'code' => $otpCode,
+                'mobile' => $this->mobile,
+                'expires_at' => now()->addMinutes(2),
+            ]
+        ]);
         $user = User::checkUsername($this->mobile);
-        $user->notify(new SendOtpViaSms(12345));
+//        $user->notify(new SendOtpViaSms($otpCode));
 
         $this->codeSent = true;
     }
 
-    public function login()
+    public function verify()
     {
+        $this->validateOnly('otp');
+        $sessionOtp = session('otp');
+
+        if (!$sessionOtp) {
+            $this->addError('otp', 'کد منقضی شده است. لطفاً دوباره درخواست دهید.');
+            return;
+        }
+
+        if (now()->greaterThan($sessionOtp['expires_at'])) {
+            session()->forget('otp');
+            $this->addError('otp', 'کد منقضی شده است.');
+            return;
+        }
+
+        if ($this->otp != $sessionOtp['code']) {
+            $this->addError('otp', 'کد وارد شده نادرست است.');
+            return;
+        }
+
+        session()->forget('otp');
+        session()->flash('message', 'کد تایید شد!');
         session()->put('tableId', 1);
-        return redirect()->to('/');
+
+        auth()->login(User::where('mobile', $sessionOtp['mobile'])->first());
+        return redirect()->route('home');
+
     }
 
     public function render()
--- resources/views/livewire/confirm-code.blade.php
+++ resources/views/livewire/confirm-code.blade.php
@@ -1,3 +1,18 @@
-<div>
-    Confirm code
-</div>
+<form class="flex flex-col mb-10 items-center justify-center"  wire:submit="verify">
+    <div class="text-red-900 text-center p-4 font-dastnevis">@error('otp') {{ $message }} @enderror</div>
+
+    <input
+        wire:model="otp"
+        class="no-spinner placeholder-lime-900 font-dastnevis rounded-xl bg-gray-100 w-80  text-2xl text-center outline-0 p-4 pt-4 shadow-2xl text-lime-900"
+        dir="rtl"
+        type="number"
+        inputmode="numeric"
+        minlength="5"
+        maxlength="5"
+        placeholder="کد تایید ۵ رقمی">
+
+    <button wire:loading.remove class="border-1 font-dastnevis rounded-xl bg-gray-100 w-80 p-4  pt-4 mt-2 shadow-2xl hover:bg-gray-200 text-lime-900 text-xl">
+        تایید
+    </button>
+    <span wire:loading class="loading loading-dots loading-lg pt-2 text-white"></span>
+</form>
