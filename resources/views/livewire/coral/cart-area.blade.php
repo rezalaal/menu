b94669aea7173daab74aaba3bd9fcfae366b4e58
Reza Lael <rezalaal@gmail.com>
2025-06-24 13:13:06 +0330

    refactor a method

--- resources/views/livewire/coral/cart-area.blade.php
+++ resources/views/livewire/coral/cart-area.blade.php
@@ -6,7 +6,7 @@
                 <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
             </svg>
             <span wire:loading class="font-dastnevis">لطفا صبر کنید.</span>
-            <span id="cart-number" class="farsi-number font-iransans-bold absolute left-2 top-1 text-[9px]">15</span>
+            <span id="cart-number" class="farsi-number font-iransans-bold absolute left-2 top-1 text-[9px]"></span>
         </button>        
     </div>
     <div id="cart-modal" class="fixed inset-0 bg-white z-50 flex-col max-h-screen overflow-y-auto px-6 py-10 hidden">
@@ -27,9 +27,11 @@
         cartNumberEl = document.querySelector("#cart-number")
         const cartData = localStorage.getItem('cart');
         if (cartData) {
-            cartNumberEl.innerText = JSON.parse(cartData).length;
+            const cartItems = JSON.parse(cartData);
+            const cartCount = cartItems.length;
+            cartNumberEl.innerText = cartCount !== 0 ? cartCount : '';
         } else {
-            cartNumberEl.innerText = 0;
+            cartNumberEl.innerText = '';
         }
         
     }, 1000)
