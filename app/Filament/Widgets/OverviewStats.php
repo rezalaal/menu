0a5efba3422fa2ac8c18c0e8802a31e83f54c57c
Reza Lael <rezalaal@gmail.com>
2025-06-26 19:26:18 +0330

    add some widgets to filament dashboard

--- app/Filament/Widgets/OverviewStats.php
+++ app/Filament/Widgets/OverviewStats.php
@@ -3,11 +3,12 @@
 namespace App\Filament\Widgets;
 
 use App\Models\Category;
+use App\Models\Order;
 use App\Models\Product;
 use App\Models\User;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Card;
-
+use Illuminate\Support\Carbon;
 class OverviewStats extends BaseWidget
 {
     protected function getCards(): array
@@ -26,6 +27,19 @@ class OverviewStats extends BaseWidget
             Card::make('تعداد مشتریان', User::count() -1 )
                 ->description('کل کاربران  ثبت نام شده')
                 ->icon('heroicon-o-user')
+                ->color('success'),
+
+            Card::make('تعداد سفارشات', Order::count() -1 )
+                ->description('تعداد کل سفارش ها ')
+                ->icon('heroicon-o-shopping-bag')
+                ->color('success'),
+            Card::make('تعداد سفارشات امروز', \App\Models\Order::whereDate('created_at', Carbon::today())->count())
+                ->description('تعداد سفارشات ثبت شده در امروز')
+                ->icon('heroicon-o-shopping-bag')
+                ->color('success'),
+            Card::make('تعداد میزها', User::count() -1 )
+                ->description('تعداد میزهای سالن')
+                ->icon('heroicon-o-clipboard')
                 ->color('success'),
         ];
     }
