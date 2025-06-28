f62fbd0de42979a034a726c7da510e4627140871
Reza Lael <rezalaal@gmail.com>
2025-06-28 21:05:10 +0330

    organize settings resource

--- app/Filament/Pages/ManageGeneralSettings.php
+++ app/Filament/Pages/ManageGeneralSettings.php
@@ -8,6 +8,7 @@ use Filament\Forms\Form;
 use Filament\Pages\SettingsPage;
 use App\Settings\GeneralSettings;
 use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Tabs;
 
 class ManageGeneralSettings extends SettingsPage
 {
@@ -15,37 +16,55 @@ class ManageGeneralSettings extends SettingsPage
 
     protected static string $settings = GeneralSettings::class;
 
-    protected static ?string $navigationLabel = "تنظیمات";
+    protected static ?string $navigationGroup = 'تنظیمات';
 
-    protected static ?string $title = "تنظیمات عمومی";
+    protected static ?string $navigationLabel = "اطلاعات پایه";
+
+    protected static ?string $title = "اطلاعات پایه";
 
     public function form(Form $form): Form
     {
         return $form
             ->schema([
-                TextInput::make('init_site_name')
-                    ->label('عنوان')
-                    ->required(),
-                TextInput::make('instagram_id')
-                    ->label('آیدی اینستاگرام')
-                    ->suffix('@')
-                    ->required(),
-                TextInput::make('master_mobile')
-                    ->label('تلفن همراه مدیر')
-                    ->required(),
-                MarkdownEditor::make('about')
-                    ->label('درباره ما')
+                Tabs::make('تنظیمات')
                     ->columnSpanFull()
-                    ->helperText('متن را می‌توانید با فرمت Markdown وارد کنید.'),
-
-                MarkdownEditor::make('contact')
-                    ->label('اطلاعات تماس')
-                    ->columnSpanFull()
-                    ->helperText('شماره تماس، آدرس، ایمیل و ...'),
-
-                MarkdownEditor::make('work_hours')
-                    ->label('ساعات کاری')
-                    ->columnSpanFull(),
+                    ->extraAttributes(['class' => 'justify-end'])
+                    ->tabs([
+                        Tabs\Tab::make('اطلاعات پایه')
+                            ->schema([
+                                TextInput::make('init_site_name')
+                                    ->label('عنوان')
+                                    ->required(),
+                                TextInput::make('instagram_id')
+                                    ->label('آیدی اینستاگرام')
+                                    ->suffix('@')
+                                    ->required(),
+                                TextInput::make('master_mobile')
+                                    ->label('تلفن همراه مدیر')
+                                    ->required(),
+                            ]),
+                        Tabs\Tab::make('درباره ما')
+                            ->schema([
+                                MarkdownEditor::make('about')
+                                    ->label('درباره ما')
+                                    ->helperText('متن را می‌توانید با فرمت Markdown وارد کنید.')
+                                    ->columnSpanFull(),
+                            ]),
+                        Tabs\Tab::make('اطلاعات تماس')
+                            ->schema([
+                                MarkdownEditor::make('contact')
+                                    ->label('اطلاعات تماس')
+                                    ->helperText('شماره تماس، آدرس، ایمیل و ...')
+                                    ->columnSpanFull(),
+                            ]),
+                        Tabs\Tab::make('ساعات کاری')
+                            ->schema([
+                                MarkdownEditor::make('work_hours')
+                                    ->label('ساعات کاری')
+                                    ->columnSpanFull(),
+                            ]),
+                    ])
             ]);
     }
+
 }
