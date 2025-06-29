b0c3e9a4e11205160f6bccd1ac0ef13e5ac45274
Reza Lael <rezalaal@gmail.com>
2025-06-29 12:31:01 +0330

    improved ui

--- resources/views/livewire/coral/user-area.blade.php
+++ resources/views/livewire/coral/user-area.blade.php
@@ -1,12 +1,16 @@
 <div>
     <button
         wire:click="profile"
+        
         class="fixed flex justify-center items-center bottom-24 left-0 rounded-tr-xl bg-coral text-white p-3 shadow-lg hover:bg-orange-500 transition"
     >
         <!-- آیکون -->
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+        <svg wire:loading.remove xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
             <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
         </svg>
         
+        <svg wire:loading xmlns="http://www.w3.org/2000/svg" fill="red" viewBox="0 0 24 24" stroke-width="1.5" stroke="red" class="size-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+        </svg>
     </button>
 </div>
